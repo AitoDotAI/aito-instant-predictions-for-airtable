64792d7486b289c60a3688e07e128a4e3297c1d5
Christoffer Ventus <christoffer.ventus@aito.ai>
2021-10-26 09:47:05 +0300

    Make the quota exceeded message a little more vague

--- frontend/components/QueryQuotaExceeded.tsx
+++ frontend/components/QueryQuotaExceeded.tsx
@@ -3,8 +3,7 @@ import React from 'react'
 
 const QueryQuotaExceeded = (props: Parameters<typeof Text>[0]) => (
   <Text {...props} variant="paragraph">
-    Query quota exeeded. The query count is reset on the first day of each month. You can also increase your quota in
-    the{' '}
+    Query quota exeeded. The query count is reset once a month. You can also increase your quota in the{' '}
     <Link target="_blank" href="https://console.aito.ai">
       Aito.ai console
     </Link>
