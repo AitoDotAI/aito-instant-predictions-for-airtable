a5097bcf2b239375c25f19e75f8a2bdde6a902a2
Christoffer Ventus <christoffer.ventus@aito.ai>
2022-03-30 11:14:11 +0300

    Add common footer to predict/train views

--- frontend/components/TableView.tsx
+++ frontend/components/TableView.tsx
@@ -1,5 +1,5 @@
 import { Cursor, Table, View } from '@airtable/blocks/models'
-import { Box, Text, Button, Loader } from '@airtable/blocks/ui'
+import { Box, Text, Button, Loader, Tooltip, Icon } from '@airtable/blocks/ui'
 import React from 'react'
 import AitoClient from '../AitoClient'
 import { UploadResult } from '../functions/uploadView'
@@ -27,29 +27,47 @@ const TableView: React.FC<{
     )
   }
 
+  const view = tableConfig.airtableViewId ? table.getViewByIdIfExists(tableConfig.airtableViewId) : null
+  const viewName = view ? view.name : undefined
+  const lastUpdated = tableConfig && tableConfig.lastRowCount && tableConfig.lastUpdated
+  const hasUploaded = Boolean(lastUpdated)
+
+  const footer = (
+    <Footer
+      viewName={viewName}
+      lastRowCount={tableConfig.lastRowCount}
+      buttonVariant={tab === 'predict' ? 'primary' : 'secondary'}
+      lastUpdated={lastUpdated ? new Date(lastUpdated) : undefined}
+      lastUploadedBy={tableConfig.lastUpdatedBy?.name}
+      buttonDisabled={tab === 'predict' && !canUpdateSettings}
+      buttonText={tab === 'predict' ? `${hasUploaded ? 'Retrain' : 'Train'} model` : 'Cancel'}
+      onButtonClick={tab === 'predict' ? () => setTab('train') : () => setTab('predict')}
+      buttonKey={tab}
+    />
+  )
+
   if (tab === 'train') {
     return (
       <Box display="flex" flexDirection="column" minHeight="100vh">
-        <React.Suspense fallback={<Spinner />}>
-          <UploadView
-            key={table.id}
-            table={table}
-            tableConfig={tableConfig}
-            onUpload={onUpload}
-            setTableConfig={setTableConfig}
-            canUpdateSettings={canUpdateSettings}
-            client={client}
-            setTab={setTab}
-          />
-        </React.Suspense>
+        <Box flexGrow={1} flexBasis="100%">
+          <React.Suspense fallback={<Spinner />}>
+            <UploadView
+              key={table.id}
+              table={table}
+              tableConfig={tableConfig}
+              onUpload={onUpload}
+              setTableConfig={setTableConfig}
+              canUpdateSettings={canUpdateSettings}
+              client={client}
+              setTab={setTab}
+            />
+          </React.Suspense>
+        </Box>
+        {footer}
       </Box>
     )
   } else {
     // tab === 'predict
-    const view = tableConfig.airtableViewId ? table.getViewByIdIfExists(tableConfig.airtableViewId) : null
-    const lastUpdated = tableConfig && tableConfig.lastRowCount && tableConfig.lastUpdated
-    const hasUploaded = Boolean(lastUpdated)
-
     return (
       <Box display="flex" flexDirection="column" minHeight="100vh">
         <Box flexGrow={1} flexBasis="100%">
@@ -64,29 +82,7 @@ const TableView: React.FC<{
             />
           </React.Suspense>
         </Box>
-        <Box padding={1} borderTop="thick" display="flex" backgroundColor="#f0f0f0" flexGrow={0}>
-          <Text variant="paragraph" flexGrow={1} size="default" padding={1} margin={0}>
-            {lastUpdated ? (
-              <>
-                {tableConfig.lastRowCount} records from <em>{view ? view.name : 'a removed view'}</em> uploaded at{' '}
-                {new Date(lastUpdated).toLocaleDateString()}
-              </>
-            ) : (
-              'No training data has been uploaded yet.'
-            )}
-          </Text>
-          <Button
-            flexGrow={0}
-            size="small"
-            style={{ height: 'auto' }}
-            alignSelf="stretch"
-            onClick={() => setTab('train')}
-            variant="primary"
-            disabled={!canUpdateSettings}
-          >
-            {hasUploaded ? 'Retrain' : 'Train'} model
-          </Button>
-        </Box>
+        {footer}
       </Box>
     )
   }
@@ -95,6 +91,76 @@ const TableView: React.FC<{
 const Spinner: React.FC = () => (
   <Box padding={3} display="flex" justifyContent="center" alignItems="center">
     <Loader scale={0.3} />
+  </Box>
+)
+
+const Footer: React.FC<{
+  lastUpdated: Date | undefined
+  lastRowCount: number | undefined
+  lastUploadedBy: string | undefined
+  viewName: string | undefined
+  buttonText: string
+  buttonDisabled: boolean
+  buttonVariant: 'primary' | 'secondary'
+  onButtonClick: () => void
+  buttonKey: string
+}> = ({
+  lastUpdated,
+  lastRowCount = 0,
+  lastUploadedBy,
+  viewName,
+  buttonVariant,
+  buttonText,
+  buttonDisabled,
+  onButtonClick,
+  buttonKey,
+}) => (
+  <Box
+    padding={1}
+    borderTop="thick"
+    display="flex"
+    backgroundColor="#f0f0f0"
+    flexGrow={0}
+    justifyContent="space-between"
+  >
+    <Tooltip
+      style={{ height: 'auto', width: '300px', maxWidth: '300px', whiteSpace: 'normal' }}
+      fitInWindowMode={Tooltip.fitInWindowModes.NUDGE}
+      placementX={Tooltip.placements.LEFT}
+      placementY={Tooltip.placements.TOP}
+      disabled={!lastUpdated}
+      content={() => (
+        <Text margin={2} textColor="white">
+          {lastUploadedBy || 'Somebody'} uploaded {lastRowCount} records from <em>{viewName || 'an old view'}</em> at{' '}
+          {lastUpdated ? new Date(lastUpdated).toLocaleString() : 'some point in time'}.
+        </Text>
+      )}
+    >
+      <Box flexGrow={0}>
+        <Text variant="paragraph" size="default" padding={1} margin={0}>
+          {lastUpdated ? (
+            <>
+              <Icon name="info" style={{ verticalAlign: 'text-bottom' }} marginRight={1} />
+              Records last uploaded {lastUpdated.toLocaleDateString()}
+            </>
+          ) : (
+            'No data has been uploaded for this table yet'
+          )}
+        </Text>
+      </Box>
+    </Tooltip>
+    <Button
+      flexGrow={0}
+      size="small"
+      style={{ height: 'auto' }}
+      alignSelf="stretch"
+      onClick={onButtonClick}
+      variant={buttonVariant}
+      disabled={buttonDisabled}
+      key={buttonKey}
+    >
+      {buttonText}
+    </Button>
   </Box>
 )
 
