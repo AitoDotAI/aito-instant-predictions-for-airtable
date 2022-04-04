c07e50a62d2f5c8852e501b295ba1979c0806f94
Christoffer Ventus <christoffer.ventus@aito.ai>
2022-04-04 14:36:56 +0300

    Add describeTasks() for planning upload tasks

--- frontend/functions/uploadView.ts
+++ frontend/functions/uploadView.ts
@@ -1,7 +1,8 @@
-import { Field, FieldType, View } from '@airtable/blocks/models'
+import { Base, Field, FieldType, View } from '@airtable/blocks/models'
 import _ from 'lodash'
-import AcceptedFields from '../AcceptedFields'
+import AcceptedFields, { isIgnoredField } from '../AcceptedFields'
 import AitoClient, { AitoError, isAitoError, Value } from '../AitoClient'
+import { TableSchema } from '../schema/aito'
 import { TableColumnMap } from '../schema/config'
 import inferAitoSchema, { mapColumnNames } from './inferAitoSchema'
 
@@ -16,6 +17,197 @@ interface SuccessfulUploadResult {
 interface ErrorUploadResult {
   type: 'error'
   error: AitoError
+}
+
+const makeLinkTableSchema = (fromTableName: string, toTableName: string): TableSchema => {
+  return {
+    type: 'table',
+    columns: {
+      [fromTableName]: {
+        type: 'String',
+        nullable: false,
+        link: `${fromTableName}.id`,
+      },
+      [toTableName]: {
+        type: 'String',
+        nullable: false,
+        link: `${toTableName}.id`,
+      },
+    },
+  }
+}
+
+type Status = 'pending' | 'in-progress' | 'done' | 'error'
+
+export interface TableInfo {
+  aitoTable: string
+  fieldIdToName: TableColumnMap
+  schema: TableSchema
+}
+
+export interface TaskCommon {
+  status: Status
+  progress?: number
+}
+
+export interface CreateTableTask extends TaskCommon {
+  type: 'create-table'
+  tableInfo: TableInfo
+  tableId: string
+  viewId: string
+}
+
+export interface CreateLinkTask extends TaskCommon {
+  type: 'create-link'
+  tableInfo: TableInfo
+  tableId: string
+  viewId: string
+  linkFieldId: string
+}
+
+export interface UploadTableTask extends TaskCommon {
+  type: 'upload-table'
+  tableInfo: TableInfo
+  fieldIds: string[]
+  tableId: string
+  viewId: string
+  records?: number
+}
+
+export interface UploadLinkTask extends TaskCommon {
+  type: 'upload-link'
+  tableInfo: TableInfo
+  tableId: string
+  viewId: string
+  linkFieldId: string
+  records?: number
+}
+
+export type UploadTask = CreateTableTask | CreateLinkTask | UploadTableTask | UploadLinkTask
+
+interface LinkInfo {
+  linkFieldId: string
+  tableId: string
+  viewId: string
+  fields: Field[]
+  aitoTable: string
+}
+
+const cloneTasks = (tasks: UploadTask[]): UploadTask[] => tasks.map((x) => ({ ...x }))
+
+export function describeTasks(
+  mainTableId: string,
+  mainViewId: string,
+  mainViewFields: Field[],
+  aitoTable: string,
+  linkViews: LinkInfo[],
+): UploadTask[] {
+  // create new table with new schema
+  const mainFields: Field[] = mainViewFields
+  const fieldIdToName = mapColumnNames(mainFields)
+  const mainTableInfo: TableInfo = {
+    aitoTable,
+    fieldIdToName,
+    schema: inferAitoSchema(mainFields, fieldIdToName),
+  }
+  const createTableTasks: CreateTableTask[] = [
+    {
+      type: 'create-table',
+      status: 'pending',
+      tableInfo: mainTableInfo,
+      tableId: mainTableId,
+      viewId: mainViewId,
+    },
+  ]
+  const uploadTableTasks: UploadTableTask[] = [
+    {
+      type: 'upload-table',
+      status: 'pending',
+      fieldIds: mainFields.map((field) => field.id),
+      tableInfo: mainTableInfo,
+      tableId: mainTableId,
+      viewId: mainViewId,
+    },
+  ]
+
+  const createLinkTasks: CreateLinkTask[] = []
+  const uploadLinkTasks: UploadLinkTask[] = []
+
+  const createdViewsSet = new Set<string>()
+  createdViewsSet.add(mainViewId)
+
+  // Create schema for each linked table and for each link field
+  for (const link of linkViews) {
+    const { linkFieldId, tableId, viewId, fields, aitoTable } = link
+
+    if (!createdViewsSet.has(viewId)) {
+      // Create schema for referenced view
+      createdViewsSet.add(viewId)
+
+      const fieldIdToName = mapColumnNames(fields)
+      const name = aitoTable
+      const schema = inferAitoSchema(fields, fieldIdToName)
+
+      const tableInfo: TableInfo = {
+        aitoTable: name,
+        fieldIdToName,
+        schema: schema,
+      }
+
+      // register tasks
+      createTableTasks.push({
+        type: 'create-table',
+        tableInfo,
+        tableId,
+        viewId,
+        status: 'pending',
+      })
+      uploadTableTasks.push({
+        type: 'upload-table',
+        tableInfo,
+        tableId,
+        fieldIds: fields.map((field) => field.id),
+        viewId,
+        status: 'pending',
+      })
+    }
+
+    const field = mainFields.find((field) => field.id === linkFieldId)
+    if (!field) {
+      throw new Error('Something went badly wrong')
+    }
+    const config = field.config
+    if (config.type !== FieldType.MULTIPLE_RECORD_LINKS) {
+      throw new Error('Something went badly wrong')
+    }
+
+    const name = aitoTable + '_L'
+    const schema = makeLinkTableSchema(mainTableId, config.options.linkedTableId)
+    const tableInfo: TableInfo = {
+      aitoTable: name,
+      schema,
+      fieldIdToName: {},
+    }
+
+    createLinkTasks.push({
+      type: 'create-link',
+      tableId: mainTableId,
+      viewId: mainViewId,
+      linkFieldId,
+      tableInfo,
+      status: 'pending',
+    })
+    uploadLinkTasks.push({
+      type: 'upload-link',
+      tableId: mainTableId,
+      viewId: mainViewId,
+      linkFieldId,
+      tableInfo,
+      status: 'pending',
+    })
+  }
+
+  return [...createTableTasks, ...createLinkTasks, ...uploadTableTasks, ...uploadLinkTasks]
 }
 
 export async function uploadView(client: AitoClient, view: View, aitoTable: string): Promise<UploadResult> {
