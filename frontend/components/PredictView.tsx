ec5c42514b8dfadea7235fac48b54d07846c9f53
Christoffer Ventus <christoffer.ventus@aito.ai>
2022-05-06 11:07:02 +0300

    Move renderCellDefault to own file

--- frontend/components/PredictView.tsx
+++ frontend/components/PredictView.tsx
@@ -40,6 +40,7 @@ import Spinner from './Spinner'
 import useEqualValue from './useEqualValue'
 import { BORDER_STYLE, InlineFieldIcon, InlineIcon } from './ui'
 import WithTableSchema from './WithTableSchema'
+import renderCellDefault from './renderCellDefault'
 
 const DEFAULT_CONFIDENCE_THRESHOLD = 90
 
@@ -457,36 +458,6 @@ const isMultipleSelectField = (field: Field): boolean => Boolean(AcceptedFields[
 
 type QueryType = 'predict' | 'match'
 const queryType = (field: Field): QueryType => (field.type === FieldType.MULTIPLE_RECORD_LINKS ? 'match' : 'predict')
-
-const renderCellDefault = (field: Field) => {
-  const RenderCell = (cellValue: unknown): React.ReactElement => {
-    if (field.type === FieldType.SINGLE_COLLABORATOR || field.type === FieldType.MULTIPLE_COLLABORATORS) {
-      return (
-        <Box marginLeft={2}>
-          <i>Unknown collaborator</i>
-        </Box>
-      )
-    }
-    if (field.type === FieldType.MULTIPLE_RECORD_LINKS) {
-      return (
-        <Box marginLeft={2}>
-          <i>Unknown record</i>
-        </Box>
-      )
-    }
-    let value: string = String(cellValue)
-    try {
-      const af = AcceptedFields[field.type]
-      if (af) {
-        value = af.cellValueToText(cellValue, field.config)
-      }
-    } catch {
-      // Ignore
-    }
-    return <i>{value}</i>
-  }
-  return RenderCell
-}
 
 const makeWhereClause = (selectedField: Field, fields: Field[], schema: TableSchema, record: Record) => {
   const fieldIdToName = mapColumnNames(fields)
